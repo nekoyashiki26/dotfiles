b6b5d35690d236d17ecce47ad7540cccf08d45e8
YoshinoriYamaguchi <b1518292@planet.kanazawa-it.ac.jp>
2018-04-26 17:23:02 +0900

    changed
    Former-commit-id: b1e042ed592a5990acaa9a40ec257c179c48c7de

---
+++ AutoProxy.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+switchtolocation() {
+  currentlocation=`networksetup -getcurrentlocation`
+  if test $currentlocation = $1; then
+    return
+  fi
+  scselect `scselect|grep ${1}|cut -b 4-40`
+  osascript -e 'display notification "'"ネットワーク環境を「${1}」へ変更しました。"'" with title "'"${0##*/}"'" subtitle "'"${2}"'"'
+}
+
+ADAPTER="en0"
+
+airportpower=`networksetup -getairportpower ${ADAPTER}|cut -d' ' -f4`
+if test ${airportpower} = 'Off'; then
+  echo 'Wifi is Off.'
+  exit
+fi
+
+SSID=`networksetup -getairportnetwork ${ADAPTER}|cut -d':' -f2|cut -b 2-`
+case "$SSID" in
+  "nekoyashiki26")
+    switchtolocation "MY-HOME" ${ssid}
+    ;;
+  "mlab")
+    switchtolocation "KIT-WLAP2" ${ssid}
+    ;;
+   "KIT-WLAP2")
+    switchtolocation "KIT-WLAP2" ${ssid}
+    ;;
+  *)
+    switchtolocation "Automatic" ${ssid}
+    ;;
+esac

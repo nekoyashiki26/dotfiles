eb55e359a47baa5f0bf6e72fa8f70eea814ffc2c
Yoshinori Yamaguchi <b1518292@planet.kanazawa-it.ac.jp>
2019-02-21 23:58:13 +0900

    CCCC aliasを変更

--- install.sh
+++ install.sh
@@ -9,6 +9,7 @@ git submodule init
 git submodule update
 
 echo "start setup..."
+
 for f in .??*; do
     [ "$f" = ".git" ] && continue
     [ "$f" = ".gitconfig.local.template" ] && continue
@@ -18,10 +19,38 @@ for f in .??*; do
     [ "$f" = ".gitattributes" ] && continue
     [ "$f" = ".git-crypt" ] && continue
 
-    ln -snfv "$THIS_DIR"/"$f" ~
+    ln -snfv "$THIS_DIR"/"$f" ~ 1>/dev/null
 done
-ln -snfv "$THIS_DIR"/nvim ~/.config
-ln -snfv "$THIS_DIR"/settings.json ~/Library/Application\ Support/Code/User
+ln -snfv "$THIS_DIR"/nvim ~/.config 1>/dev/null
+ln -snfv "$THIS_DIR"/settings.json ~/Library/Application\ Support/Code/User 1>/dev/null
+
+GIT_CONFIG_LOCAL=~/.gitconfig.local
+if [ ! -e $GIT_CONFIG_LOCAL ]; then
+    echo -n "git config user.name? > "
+    read GIT_AUTHOR_NAME
+
+    echo -n "git config user.email? > "
+    read GIT_AUTHOR_EMAIL
+
+    cat << END > $GIT_CONFIG_LOCAL
+[alias]
+    co = checkout
+    st = status
+    cm = commit -m
+    br = branch
+    cg = config
+[user]
+    naem = $GIT_AUTHOR_NAME
+    email = $GIT_AUTHOR_EMAIL
+[core]
+    editor = vim -c \"set fenc=utf-8\"
+[ghq]
+    root = ~/ghq
+    root = ~/go/src
+END
+
+fi
+
 cat << END
 
 **************************************************

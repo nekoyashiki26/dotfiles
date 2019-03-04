5e45a6805628bf017ed14cae207fec1fb90ee41e
Yoshinori Yamaguchi <b1518292@planet.kanazawa-it.ac.jp>
2019-03-04 22:47:22 +0900

    AAAA alias

--- install.sh
+++ install.sh
@@ -8,8 +8,9 @@ cd $THIS_DIR
 git submodule init
 git submodule update
 
-echo "start setup..."
+echo "start setup"
 
+# 各種設定のリンクをホームディレクトリに作成
 for f in .??*; do
     [ "$f" = ".git" ] && continue
     [ "$f" = ".gitconfig.local.template" ] && continue
@@ -24,14 +25,21 @@ done
 ln -snfv "$THIS_DIR"/nvim ~/.config 1>/dev/null
 ln -snfv "$THIS_DIR"/settings.json ~/Library/Application\ Support/Code/User 1>/dev/null
 
+# gitの設定ファイルを作成
 GIT_CONFIG_LOCAL=~/.gitconfig.local
 if [ ! -e $GIT_CONFIG_LOCAL ]; then
-    echo -n "git config user.name? > "
-    read GIT_AUTHOR_NAME
-
-    echo -n "git config user.email? > "
-    read GIT_AUTHOR_EMAIL
-
+    read -p "Are you Yoshinori Yamaguchi? [y/n] > " flag
+    
+    if [ $flag = "y" ]; then
+        GIT_AUTHOR_NAME="YoshinoriYamaguchi"
+        GIT_AUTHOR_EMAIL="b1518292@planet.kanazawa-it.ac.jp"
+    elif [ $flag = "n" ]; then
+        read -p "git config user.name? > " GIT_AUTHOR_NAME
+        read -p "git config user.email? > " GIT_AUTHOR_EMAIL
+    else
+        echo -n "error"
+        exit
+    fi
     cat << END > $GIT_CONFIG_LOCAL
 [alias]
     co = checkout
@@ -39,6 +47,13 @@ if [ ! -e $GIT_CONFIG_LOCAL ]; then
     cm = commit -m
     br = branch
     cg = config
+    bra = branch -a -vv
+    ls = ls-files
+    aliases = !git config --get-regexp alias
+    pr = !hub browse -- pull/$(git symbolic-ref --short HEAD)
+    list = !hub browse -- pulls/$(git config user.name)
+    aliases = !git config --get-regexp alias |  sed 's/^alias.//g' | sed 's/ / = /1'
+    
 [user]
     naem = $GIT_AUTHOR_NAME
     email = $GIT_AUTHOR_EMAIL
@@ -51,10 +66,5 @@ END
 
 fi
 
-cat << END
+echo -e "finish setup"
 
-**************************************************
-DOTFILES SETUP FINISHED! bye.
-**************************************************
-
-END

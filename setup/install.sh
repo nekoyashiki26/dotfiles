78077b454ae399570288917f41a776ef760ab4ca
Yoshinori Yamaguchi <b1518292@planet.kanazawa-it.ac.jp>
2019-09-16 13:40:28 +0900

    FFFF fix bug

--- setup/install.sh
+++ setup/install.sh
@@ -6,7 +6,7 @@ echo " --------- General settings start ----------"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/nekoyashiki26/dotfiles/master/setup/setup_system.sh)"
 
 echo " --------- setup brew start ----------"
-su -l $USERNAME -c 'sh -c "$(curl -fsSL https://raw.githubusercontent.com/nekoyashiki26/dotfiles/master/setup/setup_brew.sh)"'
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/nekoyashiki26/dotfiles/master/setup/setup_brew.sh)"
 
 echo " --------- dotfiles setting start ----------"
 su -l $USERNAME -c 'sh -c "$(curl -fsSL https://raw.githubusercontent.com/nekoyashiki26/dotfiles/master/setup/setup_dotfiles.sh)"'
